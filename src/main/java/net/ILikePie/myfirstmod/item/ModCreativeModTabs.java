1020ca563509e527a24651b8d0ac6168eb3dc3d3
KrzysztofCiborowski <Krzysztof.Ciborowski@ssps.ie>
2024-05-17 18:53:56 +0100

    Custom Items & Creative Mode Tab

---
+++ src/main/java/net/ILikePie/myfirstmod/item/ModCreativeModTabs.java
@@ -0,0 +1,26 @@
+package net.ILikePie.myfirstmod.item;
+
+import net.ILikePie.myfirstmod.MyFirstMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MyFirstMod.MOD_ID);
+
+    public  static final RegistryObject<CreativeModeTab> MYFIRSTMOD = CREATIVE_MODE_TABS.register("myfirstmod_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+            .title(Component.translatable("creative.myfirstmod_tab"))
+            .displayItems((itemDisplayParameters, output) -> {
+                output.accept(ModItems.SAPPHIRE.get());
+                output.accept(ModItems.RAW_SAPPHIRE.get());
+            })
+            .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
